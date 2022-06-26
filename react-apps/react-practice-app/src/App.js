aa8513447031d9bceabcca6757004a9f3f239c97
@_mathken <dummy@example.com>
2022-06-26 22:06:44 +0900

    Hello Worldを実行した

--- react-apps/react-practice-app/src/App.js
+++ react-apps/react-practice-app/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+const name = "libealくん"
+const number = 1 + 1
+
+const element = 
+  (
+    <>
+      <h1>こんにちは{name}！</h1>
+      <h2>1 + 1の答えは {number} です！</h2>
+    </>
+  )
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {element}
     </div>
   );
 }
