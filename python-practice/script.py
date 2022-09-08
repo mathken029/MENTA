9d39a78466d568efaa2e57e60178cf21ccc49024
@_mathken <dummy@example.com>
2022-09-08 09:49:54 +0900

    1問目を回答しました。

---
+++ python-practice/script.py
@@ -0,0 +1,11 @@
+# 以下のサイトの内容をPythonで書き換えます
+# http://www.kitako.tokyo/lib/CTask.aspx
+
+# 1 ～ 6 の数字を 10 回表示する(上記と同じページ)
+# from random import randrange
+
+# count=0
+# while count<10:
+#   print(randrange(1, 7, 1))
+#   count+=1
+
