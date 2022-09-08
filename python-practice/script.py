b06385a4785a7024a2db0f9e30d5841155556f1f
@_mathken <dummy@example.com>
2022-09-08 10:39:51 +0900

    フォーマッタを導入しました。

--- python-practice/script.py
+++ python-practice/script.py
@@ -1,11 +1,14 @@
 # 以下のサイトの内容をPythonで書き換えます
 # http://www.kitako.tokyo/lib/CTask.aspx
 
+# 注意点：pip install 〇〇 --user にしないとコケる
+
 # 1 ～ 6 の数字を 10 回表示する(上記と同じページ)
-# from random import randrange
+from random import randrange
 
-# count=0
-# while count<10:
-#   print(randrange(1, 7, 1))
-#   count+=1
+count = 0
+while count < 10:
+    print(randrange(1, 7, 1))
+    count += 1
 
+# レーダー<http://www.kitako.tokyo/lib/CTask.aspx?id=1>
