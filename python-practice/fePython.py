000635a8ce65d697c77131ec59cc03e8e589ef9d
@_mathken <dummy@example.com>
2022-10-04 14:02:33 +0900

    デスクトップ上の画像をテンプレートマッチングで認識してクリックする(例:ゴミ箱等)を実装しました。

---
+++ python-practice/fePython.py
@@ -0,0 +1,55 @@
+# 以下の書籍の内容を実践していきます
+# 既に理解している項目については省略します
+# https://www.amazon.co.jp/%E5%BE%B9%E5%BA%95%E6%94%BB%E7%95%A5-%E5%9F%BA%E6%9C%AC%E6%83%85%E5%A0%B1%E6%8A%80%E8%A1%93%E8%80%85%E3%81%AE%E5%8D%88%E5%BE%8C%E5%AF%BE%E7%AD%96-Python%E7%B7%A8-%E7%AC%AC2%E7%89%88-%E5%BE%B9%E5%BA%95%E6%94%BB%E7%95%A5%E3%82%B7%E3%83%AA%E3%83%BC%E3%82%BA-ebook/dp/B094X8J5XV
+
+# ■1章 Pythonの基本
+
+# a = 2 * 3
+# b = "World" * 3
+# print(a, b)
+
+# squares = [1, 4, 9, 16, 25, 36]
+# print(squares[-1])
+# print(squares[0:])
+# print(squares[-5:-1])
+
+# numbers = (1, 2, 3, 4, 5, 6)
+# numbers[0] = 2
+
+# tel = {"Mary": 2048, "John": 4096}
+# tel["John"] = 8192
+# print(tel)
+
+# basket = {"Apple", "Melon", "Apple", "Water"}
+# print(basket)
+
+# fruits = ["Apple", "Melon", "Strawberry"]
+# print("Apple" in fruits)
+
+# lista = [1, 2, 5, 4, 3]
+# print(sorted(lista))
+# print(lista)
+# lista.sort()
+# print(lista)
+# del lista[1:]
+# print(lista)
+
+# stack = [1, 2, 3]
+# stack.append(4)
+# print(stack)
+# print(stack.pop())
+# print(stack)
+
+# print(list(range(1, 12, 2)))
+
+# squares = [n**2 for n in range(1, 6)]
+# print(squares)
+
+# squares = [n**2 for n in range(1, 6) if n % 2 == 1]
+# print(squares)
+
+# multiple = [i * j for i in range(1, 4) for j in range(1, 4)]
+# print(multiple)
+
+
+# ■2章 Pythonの機能
